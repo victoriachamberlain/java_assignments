d8a9e6c7986ec7ebbbf9812650e6bb813f7deb39
Victoria <torichamberlain1@gmail.com>
2020-06-01 20:45:49 -0700

    first java program

---
+++ HelloWorld.java
@@ -0,0 +1,5 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("My name is Victoria \n" + "I am 25 years old \n" + "I live in Los Angeles");
+    }
+}
